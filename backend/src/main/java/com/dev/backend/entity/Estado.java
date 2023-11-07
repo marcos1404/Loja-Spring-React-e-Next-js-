6f73037c4afe3ada3f48c87a1a99d2b1ae73453f
Marcos vinicius <mv.99123.mv@gmail.com>
2023-11-06 21:51:50 -0300

    Crud estado

---
+++ backend/src/main/java/com/dev/backend/entity/Estado.java
@@ -0,0 +1,22 @@
+package com.dev.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "estado")
+@Data
+public class Estado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String sigla;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataCriacao;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataAtualizacao;
+}
